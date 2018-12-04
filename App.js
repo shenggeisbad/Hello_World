059ccf7583a42bd46cf3305f71fee126169affa6
Sheng Ge <shengge69@hotmail.com>
2018-12-04 19:32:56 +0800

    initial commit

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#555fff',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
